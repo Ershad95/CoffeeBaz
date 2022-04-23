0562b8fc51f6da4da928ecc25af6583d1496638d
Ershad Raoufi <waveershad21@gmail.com>
2022-04-23 05:27:09 -0700

    Add BaseService

---
+++ CoffeeBaz/CoffeeBaz.Service/IService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoffeeBaz.Service
+{
+    public interface IService<Entiry>
+    {
+        bool Insert(Entiry entity);
+        bool Update(Entiry entity);
+        bool Delete(Entiry entity);
+        Entiry GetById(int id);
+    }
+}
