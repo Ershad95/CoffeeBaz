4315c75bfb52ba9d6945d6dadb5fa6db8b3314df
Ershad Raoufi <waveershad21@gmail.com>
2022-04-23 05:25:04 -0700

    Add DataRepository and Implemention

---
+++ CoffeeBaz/CoffeeBaz.Data/DataRepository/IRepository.cs
@@ -0,0 +1,29 @@
+﻿using CoffeeBaz.Data.Domain;
+
+
+namespace CoffeeBaz.Data.DataRepository
+{
+    public interface IRepository<Template> where Template : BaseEntity
+    {
+        IQueryable<Template> Entity { get; }
+
+        bool Insert(Template entity);
+        bool Update(Template entity);
+        bool Delete(Template entity);
+
+
+        bool Update(int id);
+        bool Delete(int id);
+
+        bool Insert(IList<Template> templates);
+        bool Update(IList<Template> templates);
+        bool Delete(IList<Template> templates);
+
+
+        IList<Template> GetAll(int tableId=0,
+            int productId=0,DateTime? startDate=null,
+            DateTime? endDate=null);
+        Template GetById(int id);
+
+    }
+}
---
+++ CoffeeBaz/CoffeeBaz.Data/DataRepository/Repository.cs
@@ -0,0 +1,108 @@
+﻿using CoffeeBaz.Data.Context;
+using CoffeeBaz.Data.Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoffeeBaz.Data.DataRepository
+{
+    public class Repository<T> : IRepository<T> where T : BaseEntity
+    {
+        private CoffeBazContext _coffeBazContext { get; set; }
+        private DbSet<T> _entities;
+        public Repository(CoffeBazContext coffeBazContext)
+        {
+            _coffeBazContext = coffeBazContext;
+            _entities = _coffeBazContext.Set<T>();
+        }
+        public IQueryable<T> Entity => _entities.AsQueryable();
+
+        public bool Delete(T entity)
+        {
+            try
+            {
+                _coffeBazContext.Remove(entity);
+                _coffeBazContext.SaveChangesAsync();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public bool Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(IList<T> templates)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<T> GetAll(int tableId = 0, int productId = 0, DateTime? startDate = null, DateTime? endDate = null)
+        {
+            return Entity.ToList();
+        }
+
+        public T GetById(int id)
+        {
+            return Entity.FirstOrDefault(x => x.Id == id);
+        }
+
+        public bool Insert(T entity)
+        {
+            try
+            {
+                _coffeBazContext.Add(entity);
+                _coffeBazContext.SaveChangesAsync();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public int Insert(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Insert(IList<T> templates)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(T entity)
+        {
+            try
+            {
+                _coffeBazContext.Update(entity);
+                _coffeBazContext.SaveChangesAsync();
+                return true;
+            }
+            catch (Exception)
+            {
+
+                return false;
+            }
+        }
+
+        public bool Update(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(IList<T> templates)
+        {
+            throw new NotImplementedException();
+        }
+
+       
+    }
+}
